0eeb8b211d34d68eb93d9d739e78fbe21d8554f6
csvtuda <csvtuda@users.noreply.github.com>
2023-03-28 11:08:13 +0000

    deploy: 59c92a38104458f160971b098e13aaeb3a609b96

--- assets/videos/createThumbnails.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for /R %%f in (*.mp4 *.mkv) do (
-    @echo Processing %%f...
-    REM extract first image of video file and save it as a jpg.
-    ffmpeg -y -i %%f -vframes 1 -r 1 -f image2 -update 1 "%%~dpf%%~nf.jpg" >nul 2>&1
-    @echo   Saved to %%~dpf%%~nf.jpg
-)
